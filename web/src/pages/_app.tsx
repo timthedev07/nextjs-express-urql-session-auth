eb15056e6c666b2f74156d72b19cf1a4f21dca8f
Tim <harvard90873@gmail.com>
2022-01-07 19:55:27 +0100

    more frontend stuff

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
+import { Provider, createClient } from "urql";
+import { BACKEND } from "../constants/conn";
+
+export const client = createClient({
+  url: `${BACKEND}/graphql`,
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider value={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 };
 
