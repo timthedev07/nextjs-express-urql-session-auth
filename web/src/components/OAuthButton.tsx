c4609fc62a700fb134525cd2b96a015299bdb869
Tim <harvard90873@gmail.com>
2022-01-07 19:31:56 +0100

    copy more components over

---
+++ web/src/components/OAuthButton.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import React from "react";
+import { DiscordIcon } from "../icons/oauth/DiscordIcon";
+import { GoogleIcon } from "../icons/oauth/GoogleIcon";
+import { MicrosoftIcon } from "../icons/oauth/MicrosoftIcon";
+
+type ProviderType = "google" | "discord" | "microsoft";
+
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+
+interface OAuthButtonProps {
+  provider: ProviderType;
+  href: string | null | undefined;
+  style?: React.CSSProperties;
+}
+
+const ICON_MAP = {
+  discord: DiscordIcon,
+  google: GoogleIcon,
+  microsoft: MicrosoftIcon,
+};
+
+export const OAuthButton: React.FC<OAuthButtonProps> = ({
+  provider,
+  href,
+  style,
+}) => {
+  const Icon = ICON_MAP[provider];
+  return !href ? (
+    <button>No href provided</button>
+  ) : (
+    <Link href={href} passHref>
+      <button className={`oauth-button oauth-button-${provider}`} style={style}>
+        {capitalize(provider)}
+        <Icon className="oauth-button__icon" />
+      </button>
+    </Link>
+  );
+};
